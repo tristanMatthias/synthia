7465b45c858193ee232f9ef1b217aad564285b90
Tristan Matthias <hello@tristanmatthias.com>
2019-07-23 15:43:40 -0500

    refactor: prefixed icons with icon

--- src/typescript/icons/close.ts
+++ src/typescript/icons/close.ts
@@ -1,8 +1,8 @@
 import { html } from 'lit-html';
 
-export const iconPlay = html`
+export const iconClose = html`
 <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 40 40">
   <g stroke="var(--color)" stroke-width="4">
-      <path stroke-linecap="square" d="M27.614 20l-13.228 8.614V11.386z"/>
+    <path d="M15.326 24.674l9.348-9.348M24.674 24.674l-9.348-9.348"/>
   </g>
 </svg>`
