ddc0321322d2787ead1873bb1c687d721733c056
Tristan Matthias <hello@tristanmatthias.com>
2019-08-03 00:36:31 -0500

    fix: build

--- src/typescript/components/panels/LFOSidebar/LFOSidebar.ts
+++ src/typescript/components/panels/LFOSidebar/LFOSidebar.ts
@@ -1,10 +1,11 @@
 import { Sidebar, SidebarSide } from "../../layout/Sidebar/Sidebar";
+import { html } from "lit-html";
 
 export class LFOSidebar extends Sidebar {
   title = 'LFO Settings'
   type = SidebarSide.left;
 
   get _contents() {
-    
+    return html``;
   }
 }
