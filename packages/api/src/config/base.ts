bf4cedc70b47813650a169b5cd9aa0895c0e39d2
Tristan Matthias <hello@tristanmatthias.com>
2019-08-08 01:32:13 -0500

    fix: client host

--- packages/api/src/config/base.ts
+++ packages/api/src/config/base.ts
@@ -16,7 +16,7 @@ config({
 
 export const CONFIG_BASE: Partial<API_CONFIG> = {
   port: parseInt(process.env.PORT!) || 4000,
-  clientHost: process.env.DB_DATABASE || 'https://synthia.app',
+  clientHost: 'https://synthia.app',
   dbConnection: {
     database: process.env.DB_DATABASE!,
     username: process.env.DB_USERNAME!,
